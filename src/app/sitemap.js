71ed99f45a4766489f1d1798f10460bec80c9d35
vinicius-vds <viniciusvds-@hotmail.com>
2025-01-01 16:15:55 -0300

    adjuste adsense

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -6,12 +6,12 @@ export default async function sitemap() {
   const noticias = await client.getAllByType('noticia');
 
   const noticiasUrls = noticias.map((noticia) => ({
-    url: `https://www.seusite.com/noticias/${noticia.uid}`,
+    url: `https://www.orubronegronews.com/noticias/${noticia.uid}`,
     lastModified: noticia.last_publication_date || new Date().toISOString(),
   }));
 
   const routes = ['', '/sobre', '/contato'].map((route) => ({
-    url: `https://www.seusite.com${route}`,
+    url: `https://www.orubronegronews.com/{route}`,
     lastModified: new Date().toISOString(),
   }));
 
---
+++ src/components/GoogleAdsense.tsx
@@ -0,0 +1,22 @@
+import Script from "next/script";
+import React from "react";
+
+type Props = {
+  pId: string;
+};
+
+const GoogleAdsense: React.FC<Props> = ({ pId }) => {
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
+  return (
+    <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
+  );
+};
+
+export default GoogleAdsense;
