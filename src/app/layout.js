ad7e986304738dcc600b1c5b86e0928c12d7fac1
vinicius-vds <viniciusvds-@hotmail.com>
2025-01-03 21:22:40 -0300

    adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,7 +41,6 @@ export default function RootLayout({ children }) {
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2748482520534761"
-          crossOrigin="anonymous"
         />
       </head>
       <body className={inter.className}>
