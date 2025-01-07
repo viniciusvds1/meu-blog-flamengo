4eb9b87ef06e57e0c588234f6f820f9d1a2f04e5
vinicius-vds <viniciusvds-@hotmail.com>
2025-01-06 21:09:25 -0300

    ts config

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
       disallow: "/private/",
-    },
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+    }
   };
 }
